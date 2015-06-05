fa3bd9e8ac01e719ecfdc45bcae496fd2a358e10
chijun <chijun@yovole.com>
2015-06-05 09:40:36 +0800

    1 mkdir python 2 mv ifconfig to sys 3 mv package, configparser to python

---
+++ transconf/model/python/__init__.py
@@ -0,0 +1,6 @@
+try:
+    from transconf.dev import ContentParser
+    from transconf.dev.plugin.base import ExtensionNode
+except:
+    from ..dev import ContentParser
+    from ..dev.plugin.base import ExtensionNode
