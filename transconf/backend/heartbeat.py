4964ad698222cae636a2b7a1655988c00b07fc6d
chijun <chijun@yovole.com>
2015-07-13 18:04:44 +0800

    1 add enabled_heartbeat table 2 heartcondition

--- transconf/backend/heartbeat.py
+++ transconf/backend/heartbeat.py
@@ -7,11 +7,39 @@ class InvalidData(Exception):
     """Raised when any data can not be found """
 
 
+class HeartBeatIsEnabled(BaseTable):
+    __tablename__ = 'enabled_heartbeat'
+    uuid = StrColumn(36)
+
+    def __init__(self, uuid):
+        self.uuid = uuid
+
+
+class HeartBeatIsEnabledBackend(HeartBeatCollectionBackend):
+    def create(self):
+        self.define_table(HeartBeatIsEnabled)
+
+    def drop(self):
+        self.undefine_table(HeartBeatIsEnabled)
+
+    def has(self, group_uuid):
+        target = self.session.query(HeartBeatIsEnabled).filter_by(group_uuid=group_uuid)
+        return True if target else False
+
+    def _ready_data(self, dic_data):
+        try:
+            record = HeartBeatIsEnabled(**dic_data)
+            target = self.session.query(HeartBeatIsEnabled).filter_by(uuid=dic_data['uuid'])
+            return (target, record)
+        except TypeError:
+            raise InvalidData(record)
+
+
 class HeartBeatCollection(BaseTable):
     __tablename__ = 'heartbeat'
 
     group_name = StrColumn(15)
-    group_name = StrColumn(10)
+    group_type = StrColumn(10)
     uuid = StrColumn(36)
     available = StrColumn(5)
 
@@ -22,7 +50,7 @@ class HeartBeatCollection(BaseTable):
         self.available = available
 
 
-class HeartBeatBackend(BaseModelDriver):
+class HeartBeatCollectionBackend(BaseModelDriver):
     def create(self):
         self.define_table(HeartBeatCollection)
 
@@ -40,10 +68,7 @@ class HeartBeatBackend(BaseModelDriver):
     def has(self, group_name, group_type):
         target = self.session.query(HeartBeatCollection).filter_by(group_name=group_name,
                                                                    group_type=group_type)
-        if target:
-            return True
-        else:
-            return False
+        return True if target else False
 
     def update(self, dic_data):
         target, record = self._ready_data(dic_data)
--- transconf/server/twisted/models/heartbeat.py
+++ transconf/server/twisted/models/heartbeat.py
@@ -2,12 +2,17 @@ __author__ = 'chijun'
 
 from transconf.common.reg import register_model, get_model
 from transconf.model import Model
-from transconf.backend.heartbeat import HeartBeatBackend
-from transconf.server.twisted.internet import SQL_ENGINE, _get_conf_members
+from transconf.backend.heartbeat import HeartBeatBackend, HeartBeatIsEnabledBackend
+from transconf.server.twisted.internet import SQL_ENGINE
+from transconf.server.twisted.internet import CONF as global_conf
+
+CONF = global_conf
 
 sql_engine = SQL_ENGINE
 
 BACKEND = HeartBeatBackend(sql_engine)
+
+CONF_BACKEND = HeartBeatIsEnabledBackend(sql_engine)
 
 
 class HeartBeatNotFound(Exception):
@@ -22,7 +27,13 @@ class HeartBeat(Model):
             }
     ]
 
-    def heartbeat(self, host, timeout=60):
+    def start(self):
+        self.heart_is_start = True
+
+    def stop(self):
+        self.heart_is_start = False
+
+    def heartbeat(self, eventer, timeout=60):
         raise NotImplementedError()
 
 
@@ -31,24 +42,31 @@ class HeartCondition(Model):
     FORM = [{'node': 'heartcond',
              'public': ['has', 'mod:heartcondition:heartbeats'],
              'public': ['register', 'mod:heartcondition:register'],
-             'public': ['unregister', 'mod:heartcondition:unregister'],
             }
     ]
 
     def start(self):
-        BACKEND.create()
-
-    def stop(self):
-        BACKEND.drop()
+        # Re-initialize sql table
+        configuare_heartbeats()
 
     def register(self, context):
-        raise NotImplementedError()
+        group_name = context.get('group_name', None)
+        group_type = context.get('group_type', None)
+        uuid = context.get('group_uuid', None)
+        available = context.get('available', None) or False
+        if group_name and group_type and uuid:
+            d = dict(group_name=group_name,
+                     group_type=group_type,
+                     uuid=uuid,
+                     available=available)
+            BACKEND.update(**d)
 
-    def unregister(self, context):
-        raise NotImplementedError()
-
-    def heartbeats(self, group_name, group_type):
-        return BACKEND.has(group_name, group_type)
+    def heartbeats(self, context):
+        group_name = context.get('group_name', None)
+        group_type = context.get('group_type', None)
+        if group_name and group_type:
+            return BACKEND.has(group_name, group_type)
+        return False
 
 
 def if_available(group_name, group_type):
@@ -61,3 +79,24 @@ def if_available(group_name, group_type):
                 raise HeartBeatNotFound(group_name, group_type)
         return __if_available
     return _if_available
+
+
+def configuare_heartbeat():
+    pass
+
+
+def configuare_heartcondition():
+    global CONF_BACKEND
+    CONF_BACKEND.drop()
+    CONF_BACKEND.create()
+    global CONF
+    @from_config(sect='controller:heartbeat:listen')
+    def get_heartbeat_members(conf)
+        return conf
+    for uuid, is_enabled in get_heartbeat_members(CONF):
+        if is_enabled:
+            d = dict(group_uuid=uuid)
+            CONF_BACKEND.update(**d)
+    global BACKEND
+    BACKEND.drop()
+    BACKEND.create()
