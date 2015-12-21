56dc95c7745f9706b6229281dc4f064b567352de
terry <Terry Xi>
2015-12-21 18:19:40 +0800

    1 modify stock app js

--- app/stackoff/app/app.js
+++ app/stackoff/app/app.js
@@ -9,3 +9,38 @@ angular.module('myApp', [
 ]).config(['$routeProvider', function($routeProvider){
     $routeProvider.otherwise('/stock')
 }]);
+
+
+function GetHttpRequest()
+{
+    if ( window.XMLHttpRequest ) // Gecko
+        return new XMLHttpRequest() ;
+    else if ( window.ActiveXObject ) // IE
+        return new ActiveXObject("MsXml2.XmlHttp") ;
+}
+
+
+function loadJS(sId, url){
+    ajaxPage(sId, url);
+}
+
+function ajaxPage(sId, url){
+    var oXmlHttp = GetHttpRequest() ;
+    oXmlHttp.open('GET', url, false);//同步操作
+    oXmlHttp.setRequestHeader("Access-Control-Allow-Origin", "*");
+    oXmlHttp.send(null);
+    includeJS(sId, oXmlHttp.responseText);
+}
+
+
+function includeJS(sId, source)
+{
+    if ( ( source != null ) && ( !document.getElementById( sId ) ) ){
+        var oHead = document.getElementsByTagName('HEAD').item(0);
+        var oScript = document.createElement( "script" );
+        oScript.type = "text/javascript";
+        oScript.id = sId;
+        oScript.text = source;
+        oHead.appendChild( oScript );
+    }
+}
